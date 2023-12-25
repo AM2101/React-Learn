e9147e2b7bf884040f3737608ae051b5560057e7
Arsh  Mittal <113285090+AM2101@users.noreply.github.com>
2023-12-25 14:33:12 +0530

    currency app first

---
+++ currencyConvertor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-3xl bg-orange-600'>currency app with api</h1>
+    </>
+  )
+}
+
+export default App
